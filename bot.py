d8bfd7b2998b9a96397d6108ebcd7b2914856e38
Full-Power751 <84960062+Full-Power751@users.noreply.github.com>
2021-06-07 17:55:34 +0530

    Add mute command

--- bot.py
+++ bot.py
@@ -138,6 +138,31 @@ async def role(ctx, role: discord.Role, user: discord.Member):
     if ctx.author.guild_permissions.administrator:
         await user.add_roles(role)
         await ctx.send(f"Successfully given {role.mention} to {user.mention}.")
+        
+@client.command(description="Mutes the specified user.")
+@commands.has_permissions(manage_messages=True)
+async def mute(ctx, member: discord.Member, *, reason=None):
+    guild = ctx.guild
+    mutedRole = discord.utils.get(guild.roles, name="Muted")
+
+    if not mutedRole:
+        mutedRole = await guild.create_role(name="Muted")
+
+        for channel in guild.channels:
+            await channel.set_permissions(mutedRole, view_channel=True, speak=False, send_messages=False, read_message_history=True, read_messages=False)
+
+    await member.add_roles(mutedRole, reason=reason)
+    await ctx.send(f"Muted {member.mention} for reason {reason}")
+    await member.send(f"You were muted in the server {guild.name} for {reason}")
+
+@client.command(description="Unmutes a specified user.")
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+    await member.remove_roles(mutedRole)
+    await ctx.send(f"Unmuted {member.mention}")
+    await member.send(f"You were unmuted in the server {ctx.guild.name}")
 
 extensions=['cogs.dm',
             'cogs.avatar'
